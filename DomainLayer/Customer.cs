72e29800afeb6603d814b92ebf8d75ab631af468
Kenni Holm <kenn5363@edu.eal.dk>
2019-02-20 09:40:28 +0100

    Skeleton added along with Customer attributes

---
+++ DomainLayer/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    public class Customer
+    {
+        string Id { get; set; }
+        string Name { get; set; }
+        string Address { get; set; }
+        string ZIP { get; set; }
+        string Town { get; set; }
+        string Telephone { get; set; }
+    }
+}
