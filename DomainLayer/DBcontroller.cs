0c334352f383b62174373d210137a6863c703914
Kenni Holm <kenn5363@edu.eal.dk>
2019-02-27 13:06:49 +0100

    RegisterCostumer() started and rest of skeleton for DBcon was setup.

--- DomainLayer/DBcontroller.cs
+++ DomainLayer/DBcontroller.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DomainLayer
 {
     public class DBcontroller
     {
+        private string ConnectionString ="Server=EALSQL1.eal.local; Database= B_DB04_2018; User Id=B_STUDENT04; Password=B_OPENDB04;";
+        public void RegisterCostumer()
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = @"INSERT INTO";
+            }
+        }
+        public bool CheckCustomer()
+        {
+            return true;
+        }
+        public void RegisterOrder()
+        {
+
+        }
     }
 }
